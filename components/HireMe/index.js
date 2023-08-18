8759c2a412b80c6e23ed1c1eb85c5351846da2aa
Pratik <pratikchoudhuri1609@gmail.com>
2023-08-19 02:46:47 +0530

    Yarn Build Bugfix and Staged Hire Me Component

---
+++ components/HireMe/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../Button";
+
+const HireMe = ({ className }) => {
+  return (
+    <div className={`${className} flex justify-center flex-wrap mob:flex-nowrap light link`}>
+        <Button classes={"bg-white text-black"} onClick={() => window.open("mailto:pratikchoudhuri1609@gmail.com")}>
+            Hire Me
+        </Button>
+    </div>
+  );
+};
+
+export default HireMe;
